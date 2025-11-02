caa59de31830dbdc488b48ce6edee5593da63153
Aditya Dhanaraj Kundu <adityadhanarajkundu@gmail.com>
2025-11-02 21:21:30 +0530

    updated the server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import connectDB from "./config/db";
+import { errorHandler } from "./middlewares/errorMiddleware";
+import transactionRoutes from "./routes/transactionRoutes";
 
 dotenv.config();
 connectDB();
@@ -11,6 +13,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use("/api/transactions", transactionRoutes);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
